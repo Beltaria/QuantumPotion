e08781fea53461e349871de856edb7b2fcec3040
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-05-23 03:01:54 +0200

    init

---
+++ src/main/java/me/bluedyaishela/quantumpotion/QuantumPotion.java
@@ -0,0 +1,17 @@
+package me.bluedyaishela.quantumpotion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuantumPotion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
